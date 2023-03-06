59855130e166ddd059055799621591ceeb1a0f65
Balram Uddhav Melkunde <102988461+BalramMEL@users.noreply.github.com>
2023-03-06 18:17:23 +0530

    Blog & contact

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const BlogCard = () => {
   return (
-    <div className="col-3">
+    
       <div className="blog-card">
         <div className="card-image">
-          <img src="images/blog-1.jpg" className="img-fluid" alt="blog" />
+          <img src="images/blog-1.jpg" className="img-fluid w-100" alt="blog" />
         </div>
         <div className="blog-content">
           <p className="date">1 Jan 2023</p>
@@ -20,7 +20,7 @@ const BlogCard = () => {
           </Link>
         </div>
       </div>
-    </div>
+    
   );
 };
 
